42d7b91051c5b802f1a91b06d6cdb1a67b3aacc4
Hajime Fujita <hajime.fujita@gmail.com>
2021-09-05 14:44:27 +0900

    Vueのコードを追加した

--- config/urls.py
+++ config/urls.py
@@ -19,7 +19,7 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name='home.html'), name='home'),
-    path('dj-rest-auth/', include('dj_rest_auth.urls')),
-    path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
+    path('', TemplateView.as_view(template_name='index.html'), name='index'),
+    path('auth/', include('dj_rest_auth.urls')),
+    path('auth/registration/', include('dj_rest_auth.registration.urls')),
 ]
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,17 +1,22 @@
-import '@babel/polyfill'
-import 'mutationobserver-shim'
 import Vue from 'vue'
-import './plugins/bootstrap-vue'
+
+// BootstrapVue
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import vuetify from './plugins/vuetify'
+import Vuetify from 'vuetify'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = process.env.NODE_ENV === 'production'
+
+Vue.use(BootstrapVue)
+Vue.use(Vuetify)
 
 new Vue({
   router,
   store,
-  vuetify,
-  render: h => h(App)
+  render: h => h(App),
 }).$mount('#app')
